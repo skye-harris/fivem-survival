629562d0a1c7f8f695c1439f27107c9907456b27
Skye Harris <skyeserenaharris@gmail.com>
2024-11-19 07:55:45 +0800

    Add client-side timeout until next use of interactable objects

--- src/client/interactiveObjects.js
+++ src/client/interactiveObjects.js
@@ -20,12 +20,14 @@ const Directions = {
 }
 
 class InteractiveObject {
-    constructor(entityId, text, useDirection, onUse, textOffset = [0, 0, 0.1]) {
+    constructor(entityId, text, useDirection, onUse, timeBetweenUses, textOffset = [0, 0, 0.1]) {
         this.entity = entityId;
         this.onUse = onUse;
         this.text = text;
         this.textOffset = textOffset;
         this.direction = useDirection;
+        this.timeBetweenUses = timeBetweenUses;
+        this.lastUsedTime = 0;
     }
 }
 
@@ -37,25 +39,29 @@ let ObjectHandlers = {
         model: 'prop_till_01',
         onUse: cashRegisterInteraction,
         text: 'Press [] to rob till',
+        timeBetweenUses: 10000,
     },
     'prop_till_02': {
         model: 'prop_till_02',
         onUse: cashRegisterInteraction,
         text: 'Press [] to rob till',
+        timeBetweenUses: 10000,
     },
 
     // Vending machines
     'prop_vend_soda_01': {
         model: 'prop_vend_soda_01',
         onUse: vendingMachineInteraction,
-        direction: Directions.Any,
+        direction: Directions.Behind,
         text: 'Press [] to buy drink',
+        timeBetweenUses: 0,
     },
     'prop_vend_soda_02': {
         model: 'prop_vend_soda_02',
         onUse: vendingMachineInteraction,
-        direction: Directions.Any,
+        direction: Directions.Behind,
         text: 'Press [] to buy drink',
+        timeBetweenUses: 0,
     },
 
     // ATMs
@@ -64,24 +70,28 @@ let ObjectHandlers = {
         onUse: (obj) => atmInteraction(obj, 1),
         textOffset: [0, 0, 1.1],
         text: 'Press [] to bank cash',
+        timeBetweenUses: 0,
     },
     'prop_atm_02': {
         model: 'prop_atm_02',
         onUse: (obj) => atmInteraction(obj, 0.5),
         textOffset: [0, 0, 1],
         text: 'Press [] to bank cash',
+        timeBetweenUses: 0,
     },
     'prop_atm_03': {
         model: 'prop_atm_03',
         onUse: (obj) => atmInteraction(obj, 0.5),
         textOffset: [0, 0, 1],
         text: 'Press [] to bank cash',
+        timeBetweenUses: 0,
     },
     'prop_fleeca_atm': {
         model: 'prop_fleeca_atm',
         onUse: (obj) => atmInteraction(obj, 0.5),
         textOffset: [0, 0, 1.25],
         text: 'Press [] to bank cash',
+        timeBetweenUses: 0,
     },
 };
 
@@ -101,27 +111,27 @@ export default function initInteractiveObjects() {
             return;
         }
 
-        let nearbyObject = [...interactiveObjects].map((obj) => {
+        const gameTimer = GetGameTimer();
+        let nearbyObject = interactiveObjects.filter((obj) => {
+            return gameTimer > obj.lastUsedTime + obj.timeBetweenUses;
+        }).map((obj) => {
             return {...obj, distance: distanceBetweenEntities(PlayerPedId(), obj.entity)};
-        })
-                                                  .filter((obj) => {
-                                                      return obj.distance <= MAX_TEXT_DRAW_DISTANCE && IsEntityVisible(obj.entity) && HasEntityClearLosToEntity(PlayerPedId(), obj.entity, 16);
-                                                  })
-                                                  .sort((a, b) => {
-                                                      return a.distance - b.distance;
-                                                  })
-                                                  .find((obj) => {
-                                                      switch (obj.direction) {
-                                                          case Directions.InFront:
-                                                              return isEntityInFrontOfEntity(PlayerPedId(), obj.entity);
+        }).filter((obj) => {
+            return obj.distance <= MAX_TEXT_DRAW_DISTANCE && IsEntityVisible(obj.entity) && HasEntityClearLosToEntity(PlayerPedId(), obj.entity, 16);
+        }).sort((a, b) => {
+          return a.distance - b.distance;
+        }).find((obj) => {
+          switch (obj.direction) {
+              case Directions.InFront:
+                  return isEntityInFrontOfEntity(PlayerPedId(), obj.entity);
 
-                                                          case Directions.Behind:
-                                                              return isEntityBehindEntity(PlayerPedId(), obj.entity);
+              case Directions.Behind:
+                  return isEntityBehindEntity(PlayerPedId(), obj.entity);
 
-                                                          default:
-                                                              return true;
-                                                      }
-                                                  });
+              default:
+                  return true;
+          }
+        });
 
         if (nearbyObject) {
             const entityCoords = GetEntityCoords(nearbyObject.entity, false);
@@ -153,12 +163,19 @@ export default function initInteractiveObjects() {
                     if (nearbyObject.distance <= MAX_USE_DISTANCE) {
                         isInteracting = true;
 
+                        // Run our callback and then cleanup
                         nearbyObject.onUse(nearbyObject)
                                     .catch((err) => {
-                                        displayTextOnScreen(err, 0.5, 0.3, 0.4, [255,64,64,255], 2000, true);
+                                        displayTextOnScreen(err, 0.5, 0.3, 0.4, [255, 64, 64, 255], 2000, true);
                                     })
                                     .finally(() => {
                                         isInteracting = false;
+
+                                        const obj = interactiveObjects.find((obj) => obj.entity === nearbyObject.entity);
+                                        if (obj) {
+                                            // Object in original array, update the next use timer
+                                            obj.lastUsedTime = GetGameTimer() + obj.timeBetweenUses;
+                                        }
                                     });
                     }
                 }
@@ -168,10 +185,10 @@ export default function initInteractiveObjects() {
         }
     });
 
-    addEventListener('interactions:register', (entity, text, direction, onUse, textOffset) => {
+    addEventListener('interactions:register', (entity, text, direction, onUse, timeBetweenUses, textOffset) => {
         interactiveObjects = interactiveObjects.filter((obj) => obj.entity !== entity);
 
-        interactiveObjects.push(new InteractiveObject(entity, text, direction, onUse, textOffset))
+        interactiveObjects.push(new InteractiveObject(entity, text, direction, onUse, timeBetweenUses, textOffset))
     });
 
     setTick(throttle(() => {
@@ -187,10 +204,11 @@ export default function initInteractiveObjects() {
                     text: objData.hasOwnProperty('text') ? objData.text : `Press [] to use`,
                     useDirection: objData.hasOwnProperty('direction') ? objData.direction : Directions.Behind,
                     onUse: objData.onUse,
-                    textOffset: objData.hasOwnProperty('textOffset') ? objData.textOffset : [0, 0, 0.1]
+                    textOffset: objData.hasOwnProperty('textOffset') ? objData.textOffset : [0, 0, 0.1],
+                    timeBetweenUses: objData.hasOwnProperty('timeBetweenUses') ? objData.timeBetweenUses : 5000,
                 };
 
-                emit('interactions:register', args.entity, args.text, args.useDirection, args.onUse, args.textOffset);
+                emit('interactions:register', args.entity, args.text, args.useDirection, args.onUse, args.timeBetweenUses, args.textOffset);
             }
         }
     }, 5000));
@@ -239,9 +257,10 @@ function cashRegisterInteraction(obj) {
                 scareNearbyPeds();
 
                 // Play the animation
-                await runAnimationLooped(playerPed, animDict, animName, 3, true).catch(() => {
-                    clearInterval(soundInterval)
-                });
+                await runAnimationLooped(playerPed, animDict, animName, 3, true)
+                    .catch(() => {
+                        clearInterval(soundInterval)
+                    });
 
                 SetCurrentPedWeapon(playerPed, currWeapon[1], true);
                 DisableControlAction(0, 37, false);
@@ -255,82 +274,84 @@ function cashRegisterInteraction(obj) {
 }
 
 function vendingMachineInteraction(obj) {
-    return new Promise((resolve,reject) => {
-        spendCash(1).then(() => {
-            const playerPed = PlayerPedId(); // Get the player's ped ID
-            const animDict = "mini@sprunk";
-            const soundSet = "HUD_LIQUOR_STORE_SOUNDSET"
-            RequestScriptAudioBank(soundSet, false);
+    return new Promise((resolve, reject) => {
+        spendCash(1)
+            .then(() => {
+                const playerPed = PlayerPedId(); // Get the player's ped ID
+                const animDict = "mini@sprunk";
+                const soundSet = "HUD_LIQUOR_STORE_SOUNDSET"
+                RequestScriptAudioBank(soundSet, false);
 
-            loadAnimationDict(animDict)
-                .then(async () => {
-                    const useCoords = getEntityPositionInFrontOrBehind(obj.entity, 1, true);
-                    const playerCoords = GetEntityCoords(playerPed, false);
+                loadAnimationDict(animDict)
+                    .then(async () => {
+                        const useCoords = getEntityPositionInFrontOrBehind(obj.entity, 1, true);
+                        const playerCoords = GetEntityCoords(playerPed, false);
 
-                    // if (IsPositionOccupied(useCoords[0], useCoords[1], playerCoords[2], 0.2, true, false, true, false, false, playerPed, false)) {
-                    //     reject(new Error("There is no room available"));
-                    //     return;
-                    // }
-                    const drinkHash = GetHashKey('prop_ld_can_01');
-                    await loadModel(drinkHash);
+                        // if (IsPositionOccupied(useCoords[0], useCoords[1], playerCoords[2], 0.2, true, false, true, false, false, playerPed, false)) {
+                        //     reject(new Error("There is no room available"));
+                        //     return;
+                        // }
+                        const drinkHash = GetHashKey('prop_ld_can_01');
+                        await loadModel(drinkHash);
 
-                    FreezeEntityPosition(playerPed, true);
-                    SetEntityCoords(playerPed, useCoords[0], useCoords[1], playerCoords[2]-1, true, true, true, false);
+                        FreezeEntityPosition(playerPed, true);
+                        SetEntityCoords(playerPed, useCoords[0], useCoords[1], playerCoords[2] - 1, true, true, true, false);
 
-                    const heading = calculateHeadingForEntityFaceEntity(playerPed, obj.entity);
-                    SetEntityHeading(playerPed, heading);
+                        const heading = calculateHeadingForEntityFaceEntity(playerPed, obj.entity);
+                        SetEntityHeading(playerPed, heading);
 
-                    const currWeapon = GetCurrentPedWeapon(playerPed, true);
-                    DisableControlAction(0, 37, true);
-                    SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true);
+                        const currWeapon = GetCurrentPedWeapon(playerPed, true);
+                        DisableControlAction(0, 37, true);
+                        SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true);
 
-                    let drinkCan = 0;
-                    let isCancelled = false;
-                    const drinkSpawnTimeout = setTimeout(async () => {
-                        drinkCan = CreateObject(drinkHash, playerCoords[0], playerCoords[1], playerCoords[2], true, false, false);
-                        SetModelAsNoLongerNeeded(drinkHash);
-                        SetObjectAsNoLongerNeeded(drinkCan);
-                        SetEntityAsNoLongerNeeded(drinkCan);
+                        let drinkCan = 0;
+                        let isCancelled = false;
+                        const drinkSpawnTimeout = setTimeout(async () => {
+                            drinkCan = CreateObject(drinkHash, playerCoords[0], playerCoords[1], playerCoords[2], true, false, false);
+                            SetModelAsNoLongerNeeded(drinkHash);
+                            SetObjectAsNoLongerNeeded(drinkCan);
+                            SetEntityAsNoLongerNeeded(drinkCan);
 
-                        if (!isCancelled) {
-                            AttachEntityToEntity(drinkCan, playerPed, GetPedBoneIndex(playerPed, 18905), 0.11, 0.01, 0.02, -100, 0, -10, true, true, false, true, 1, true);
-                            await sleep(3300);
-                        }
-
-                        if (!isCancelled) {
-                            AttachEntityToEntity(drinkCan, playerPed, GetPedBoneIndex(playerPed, 57005), 0.12, 0.01, -0.02, -70, 0, -25, true, true, false, true, 1, true);
-                            await sleep(2500);
-                        }
-
-                        DetachEntity(drinkCan, true, true);
-                    }, 3500)
-
-                    // Play the animation
-                    await runAnimation(playerPed, animDict, 'plyr_buy_drink_pt1', 0, 2, true)
-                        .then(() => runAnimation(playerPed, animDict, 'plyr_buy_drink_pt2', 0, 2, true))
-                        .then(() => runAnimation(playerPed, animDict, 'plyr_buy_drink_pt3', 0, 0, true))
-                        .catch((err) => {
-                            if (drinkCan) {
-                                DetachEntity(drinkCan, true, true);
+                            if (!isCancelled) {
+                                AttachEntityToEntity(drinkCan, playerPed, GetPedBoneIndex(playerPed, 18905), 0.11, 0.01, 0.02, -100, 0, -10, true, true, false, true, 1, true);
+                                await sleep(3300);
                             }
-                            clearTimeout(drinkSpawnTimeout);
-                        })
 
-                    setTimeout(() => {
-                        DeleteEntity(drinkCan);
-                        DeleteObject(drinkCan);
-                    }, 6000);
+                            if (!isCancelled) {
+                                AttachEntityToEntity(drinkCan, playerPed, GetPedBoneIndex(playerPed, 57005), 0.12, 0.01, -0.02, -70, 0, -25, true, true, false, true, 1, true);
+                                await sleep(2500);
+                            }
 
-                    SetCurrentPedWeapon(playerPed, currWeapon[1], true);
-                    DisableControlAction(0, 37, false);
+                            DetachEntity(drinkCan, true, true);
+                        }, 3500)
 
-                    FreezeEntityPosition(playerPed, false);
-                    ClearPedTasks(playerPed);
-                    resolve();
-                });
-        }).catch(err => {
-            reject(new Error("You cannot afford a drink"));
-        });
+                        // Play the animation
+                        await runAnimation(playerPed, animDict, 'plyr_buy_drink_pt1', 0, 2, true)
+                            .then(() => runAnimation(playerPed, animDict, 'plyr_buy_drink_pt2', 0, 2, true))
+                            .then(() => runAnimation(playerPed, animDict, 'plyr_buy_drink_pt3', 0, 0, true))
+                            .catch((err) => {
+                                if (drinkCan) {
+                                    DetachEntity(drinkCan, true, true);
+                                }
+                                clearTimeout(drinkSpawnTimeout);
+                            })
+
+                        setTimeout(() => {
+                            DeleteEntity(drinkCan);
+                            DeleteObject(drinkCan);
+                        }, 6000);
+
+                        SetCurrentPedWeapon(playerPed, currWeapon[1], true);
+                        DisableControlAction(0, 37, false);
+
+                        FreezeEntityPosition(playerPed, false);
+                        ClearPedTasks(playerPed);
+                        resolve();
+                    });
+            })
+            .catch(err => {
+                reject(new Error("You cannot afford a drink"));
+            });
     });
 }
 
@@ -344,34 +365,35 @@ function atmInteraction(obj, useDistance) {
         RequestScriptAudioBank(soundSet, false);
 
         Promise.all([loadAnimationDict(animDictEnter), loadAnimationDict(animDictExit)])
-            .then(async () => {
-                FreezeEntityPosition(playerPed, true);
-                const objCoords = getEntityPositionInFrontOrBehind(obj.entity, useDistance, true);
-                const playerCoords = GetEntityCoords(playerPed, false);
-                SetEntityCoords(playerPed, objCoords[0], objCoords[1], playerCoords[2] - 1, true, true, true, false);
+               .then(async () => {
+                   FreezeEntityPosition(playerPed, true);
+                   const objCoords = getEntityPositionInFrontOrBehind(obj.entity, useDistance, true);
+                   const playerCoords = GetEntityCoords(playerPed, false);
+                   SetEntityCoords(playerPed, objCoords[0], objCoords[1], playerCoords[2] - 1, true, true, true, false);
 
-                const heading = calculateHeadingForEntityFaceEntity(playerPed, obj.entity);
-                SetEntityHeading(playerPed, heading);
+                   const heading = calculateHeadingForEntityFaceEntity(playerPed, obj.entity);
+                   SetEntityHeading(playerPed, heading);
 
-                // Holster weapon
-                const currWeapon = GetCurrentPedWeapon(playerPed, true);
-                DisableControlAction(0, 37, true);
-                SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true);
+                   // Holster weapon
+                   const currWeapon = GetCurrentPedWeapon(playerPed, true);
+                   DisableControlAction(0, 37, true);
+                   SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true);
 
-                // Play the animation
-                await runAnimation(playerPed, animDictEnter, 'enter', 0, 2, false);
-                await runAnimation(playerPed, animDictExit, 'exit', 0, 0, false).then(() => {
-                    emitNet('skyemod:bankCash');
-                });
+                   // Play the animation
+                   await runAnimation(playerPed, animDictEnter, 'enter', 0, 2, false);
+                   await runAnimation(playerPed, animDictExit, 'exit', 0, 0, false)
+                       .then(() => {
+                           emitNet('skyemod:bankCash');
+                       });
 
-                ClearPedTasks(playerPed);
-                FreezeEntityPosition(playerPed, false);
+                   ClearPedTasks(playerPed);
+                   FreezeEntityPosition(playerPed, false);
 
-                // Reequip weapon
-                SetCurrentPedWeapon(playerPed, currWeapon[1], true);
-                DisableControlAction(0, 37, false);
+                   // Reequip weapon
+                   SetCurrentPedWeapon(playerPed, currWeapon[1], true);
+                   DisableControlAction(0, 37, false);
 
-                resolve();
-            });
+                   resolve();
+               });
     });
 }
--- src/util/util.js
+++ src/util/util.js
@@ -153,14 +153,6 @@ export function sendChat(message) {
     })
 }
 
-export function distance3D(x1, y1, z1, x2, y2, z2) {
-    return Math.abs(Math.sqrt(
-        Math.pow(x2 - x1, 2) +
-        Math.pow(y2 - y1, 2) +
-        Math.pow(z2 - z1, 2)
-    ));
-}
-
 export function distanceBetweenEntities(e1, e2) {
     const coords1 = GetEntityCoords(e1);
     const coords2 = GetEntityCoords(e2);
