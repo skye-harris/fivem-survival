b9fdcf02e0bd01db5ac038f738b0e67d74988f24
Skye Harris <skyeserenaharris@gmail.com>
2024-12-12 08:37:50 +0800

    Added /respawn command for manual respawn if required

--- src/client.js
+++ src/client.js
@@ -3,13 +3,14 @@ import initFlameTrail from "./client/flames";
 import initCopOverride from "./client/copOverride";
 import initPedControl from "./client/pedControl";
 import initAllyPed from "./client/allyPed";
-import {displayTextOnScreen, distanceBetweenEntities, drawTextThisFrame, getPlayerAimTarget, sendChat} from "./util/util";
+import {displayTextOnScreen, drawTextThisFrame, getPlayerAimTarget, sendChat} from "./util/util";
 import initAiTest from "./client/aiTest";
 import config from "./client/config";
 import {MoneySources, setPlayerCash} from "./util/Cash";
 import initSpawnManager from "./client/spawnManager";
 import {initInteractiveObjects} from "./client/interactiveObjects";
 import initUnionDepositoryHeist from "./client/Heist/UnionDepository";
+import initZombies from "./client/zombies";
 
 // init some listeners
 addNetEventListener('skyemod:setCash', (walletCash, bankCash) => {
@@ -36,19 +37,19 @@ RegisterCommand("settime", (source, args) => {
 // Handle time update
 addNetEventListener('skyemod:settime', (args) => {
     NetworkOverrideClockTime(Number(args[0]), Number(args[1]), Number(args[2]));
-    displayTextOnScreen(`Time has been set to ${args[0]}:${args[1]}:${args[2]}!`, 0.5, 0.1, 0.5, [255,255,255,200],3000, true);
+    displayTextOnScreen(`Time has been set to ${args[0]}:${args[1]}:${args[2]}!`, 0.5, 0.1, 0.5, [255, 255, 255, 200], 3000, true);
 });
 
-RegisterCommand("bptyres", (source,args) => {
+RegisterCommand("bptyres", (source, args) => {
     const vehicle = GetVehiclePedIsUsing(PlayerPedId());
     if (vehicle) {
         const numWheels = GetVehicleNumberOfWheels(vehicle);
-        for (let wheel=0;wheel < numWheels; wheel++) {
-            SetVehicleTyreFixed(vehicle,wheel);
+        for (let wheel = 0; wheel < numWheels; wheel++) {
+            SetVehicleTyreFixed(vehicle, wheel);
         }
 
         SetVehicleTyresCanBurst(vehicle, false);
-        displayTextOnScreen("Made tyres bulletproof!", 0.5, 0.1, 0.5, [255,255,255,200],3000, true);
+        displayTextOnScreen("Made tyres bulletproof!", 0.5, 0.1, 0.5, [255, 255, 255, 200], 3000, true);
     }
 });
 
@@ -66,13 +67,12 @@ initInteractiveObjects();
 initUnionDepositoryHeist();
 
 initAiTest();
+//initZombies();
 
 for (let group of config.hatedGroups) {
     SetRelationshipBetweenGroups(4, GetHashKey('PLAYER'), GetHashKey(group))
     SetRelationshipBetweenGroups(4, GetHashKey(group), GetHashKey('PLAYER'))
 }
-
-displayTextOnScreen("SkyMod started", 0, 0, 0.5, [255,255,255,200], 3000, false);
 
 let objTick = null;
 RegisterCommand("whatobj", () => {
@@ -85,11 +85,11 @@ RegisterCommand("whatobj", () => {
         objTick = setTick(() => {
             const ent = getPlayerAimTarget();
             if (ent) {
-                const coords = GetEntityCoords(ent,false);
+                const coords = GetEntityCoords(ent, false);
                 const model = GetEntityModel(ent);
 
-                for (let i=0;i<3;i++) {
-                    coords[i] = Math.round(coords[i] * 100)/100;
+                for (let i = 0; i < 3; i++) {
+                    coords[i] = Math.round(coords[i] * 100) / 100;
                 }
 
                 drawTextThisFrame(0.5, 0.45, `ModelHash: ${model}, X:${coords[0]}, Y:${coords[1]}, Z:${coords[2]}`, 0.3, true);
@@ -97,3 +97,9 @@ RegisterCommand("whatobj", () => {
         });
     }
 })
+
+RegisterCommand("respawn", () => {
+    exports.spawnmanager.spawnPlayer();
+});
+
+displayTextOnScreen("SkyMod started", 0, 0, 0.5, [255, 255, 255, 200], 3000, false);
